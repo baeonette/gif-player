8d8e736b917dfbcf7bc9b78cedff226291b60a72
baeonette <baeonette@gmail.com>
2022-09-02 01:13:40 -0700

    Update append.sh

--- backend/append.sh
+++ backend/append.sh
@@ -3,8 +3,7 @@
 
 DISPLAY=:0.0 \
 chromium-browser \
-http://localhost:3000/api/ftp/playing/$1
+http://localhost:3000/api/playing/$1
 
-# Flag documentation: https://www.systutorials.com/docs/linux/man/1-mplayer/
 # Config boot script here:
 # sudo nano /etc/rc.local
